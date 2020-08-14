278533fb776840bc12a13a2fcb6d1670225e9b20
Aremu Oluwagbamila (SMOG) <aremuoluwagbamila@gmail.com>
2020-08-13 23:46:21 -0700

    Create manual route to save projects

--- routes/api.php
+++ routes/api.php
@@ -17,5 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::post("/projects", "ProjectsController@store");
 
 Route::resource('projects','ProjectsController');
+
