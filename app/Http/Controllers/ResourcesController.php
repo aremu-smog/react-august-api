45349a5e2d41e9c7fa8931ed5ed58914868343d0
Aremu Oluwagbamila (SMOG) <aremuoluwagbamila@gmail.com>
2020-08-18 17:28:53 -0700

    Resource API

---
+++ app/Http/Controllers/ResourcesController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Resource;
+
+class ResourcesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        return Resource::all();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        $request->validate([
+            "resource_name"=>"required",
+            "resource_link"=>"required"
+        ])
+
+        Resource::create(["name"=>$request->resource_name,"link"=>$request->resource_link]);
+
+        return ["message"=>"Resource created","success":true]
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+        return Project::find($id);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+        $resource = Resource::find($id);
+
+        $resource->name = $request->resource_name;
+        $resource->link = $request->resource_link;
+
+        $resource->save();
+
+        return ["message"=>"Data updated","status"=>true];
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+        Resource::destroy($id);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,5 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::resource('projects','ProjectsController');
+Route::resource('resources','ResourcesController');
 
